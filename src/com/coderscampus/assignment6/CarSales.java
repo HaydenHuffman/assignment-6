9d57905488907c3c1051d9547ada910e6092bfcd
HaydenHuffman <120144719+HaydenHuffman@users.noreply.github.com>
2023-09-01 14:08:57 -0400

    Update CarSales.java
    Switch Date objects to YearMonth

--- src/com/coderscampus/assignment6/CarSales.java
+++ src/com/coderscampus/assignment6/CarSales.java
@@ -4,114 +4,107 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CarSales {
-	private String month;
-	private Integer numberSold;
-	
-	public CarSales(String month, Integer numberSold) {
-		this.month = month;
-		this.numberSold = numberSold;
-	}
+    private YearMonth month;
+    private Integer numberSold;
 
-	public CarSales() {
-		// TODO Auto-generated constructor stub
-	}
+    public CarSales(YearMonth month, Integer numberSold) {
+        this.month = month;
+        this.numberSold = numberSold;
+    }
 
-	public String getMonth() {
-		return month;
-	}
+    public CarSales() {
+    }
 
-	public void setMonth(String month) {
-		this.month = month;
-	}
+    public YearMonth getMonth() {
+        return month;
+    }
 
-	public Integer getNumberSold() {
-		return numberSold;
-	}
+    public void setMonth(YearMonth month) {
+        this.month = month;
+    }
 
-	public void setNumberSold(Integer numberSold) {
-		this.numberSold = numberSold;
-	}
-	
-	public List<CarSales> readFile(String file) {
-		List<CarSales> sales = new ArrayList<>();
-		try (FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-				String line;
-				bufferedReader.readLine();
-				while ((line = bufferedReader.readLine()) != null) {
-					String[] values = line.split(",");
-					CarSales carSales = new CarSales(values[0], Integer.parseInt(values[1]));
-					sales.add(carSales);
-				}
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		return sales;
-	}
-	
-	public static Integer totalSold(List<CarSales> sales, String year) {
-		Integer total = sales.stream()
-					 		 .filter(y -> y.getMonth().contains(year))
-					 		 .map(CarSales::getNumberSold)
-					 		 .mapToInt(Integer::intValue)
-					 		 .sum();
-		return total;
-	}
-	
-	public static void maxMin(List<CarSales> sales, String car) throws ParseException {
-		CarSales maxMonth = sales.stream()
-								 .max(Comparator.comparing(CarSales::getNumberSold))
-								 .orElse(null);		
-		
-		
-		
-		
-		String maxMonthString = maxMonth.getMonth();
-		SimpleDateFormat format = new SimpleDateFormat("MMM-yy");
-		SimpleDateFormat newFormat = new SimpleDateFormat("yyyy-MM");
-		Date maxDate = format.parse(maxMonthString);
-		String formattedMaxDate = newFormat.format(maxDate);
-		System.out.println();
-		System.out.println("The best month for " + car + " was: " + formattedMaxDate);
-		
-		CarSales minMonth = sales.stream()
-								 .min(Comparator.comparing(CarSales::getNumberSold))
-								 .orElse(null);
-		
-		String minMonthString = minMonth.getMonth();
-		Date minDate = format.parse(minMonthString);
-		String formattedMinDate = newFormat.format(minDate);
-		System.out.println("The worst month for " + car + " was: " + formattedMinDate);
-		System.out.println();
-			 
-		
-	}
-	
-	 public static void printYearlySalesReport(String modelName, List<CarSales> sales) {
-	        Set<String> years = sales.stream()
-	                				 .map(sale -> sale.getMonth().substring(4, 6))
-	                				 .collect(Collectors.toSet());
+    public Integer getNumberSold() {
+        return numberSold;
+    }
 
-	        years.forEach(year -> {
-	            int totalSales = CarSales.totalSold(sales, year);
-	            System.out.println("20" + year + " -> " + totalSales);
-	        });
-	    }
-	
-	
+    public void setNumberSold(Integer numberSold) {
+        this.numberSold = numberSold;
+    }
+
+    public List<CarSales> readFile(String file) {
+        List<CarSales> sales = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-yy");
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            bufferedReader.readLine();
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] values = line.split(",");
+                YearMonth carMonth = YearMonth.parse(values[0], formatter);
+                CarSales carSales = new CarSales(carMonth, Integer.parseInt(values[1]));
+                sales.add(carSales);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sales;
+    }
+
+    public static Integer totalSold(List<CarSales> sales, YearMonth yearMonth) {
+        Integer total = sales.stream()
+                .filter(sale -> sale.getMonth().getYear() == yearMonth.getYear() &&
+                                sale.getMonth().getMonthValue() == yearMonth.getMonthValue())
+                .map(CarSales::getNumberSold)
+                .mapToInt(Integer::intValue)
+                .sum();
+        return total;
+    }
+
+    public static void maxMin(List<CarSales> sales, String car) {
+        CarSales maxMonth = sales.stream()
+                .max(Comparator.comparing(CarSales::getNumberSold))
+                .orElse(null);
+
+        YearMonth maxDate = maxMonth.getMonth();
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM");
+        String formattedMaxDate = outputFormatter.format(maxDate);
+        System.out.println();
+        System.out.println("The best month for " + car + " was: " + formattedMaxDate);
+
+        CarSales minMonth = sales.stream()
+                .min(Comparator.comparing(CarSales::getNumberSold))
+                .orElse(null);
+
+        YearMonth minDate = minMonth.getMonth();
+        String formattedMinDate = outputFormatter.format(minDate);
+        System.out.println("The worst month for " + car + " was: " + formattedMinDate);
+        System.out.println();
+    }
+
+    public static void printYearlySalesReport(String modelName, List<CarSales> sales) {
+        Map<Integer, Integer> yearlySales = sales.stream()
+                .collect(Collectors.groupingBy(
+                        sale -> sale.getMonth().getYear(),
+                        Collectors.summingInt(CarSales::getNumberSold)
+                ));
+
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy");
+
+        yearlySales.forEach((year, totalSales) -> {
+            System.out.println(year + " -> " + totalSales);
+        });
+    }
 }
+
 
